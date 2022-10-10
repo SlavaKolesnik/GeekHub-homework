45f0a5d2901157e741eb1b1cb6a387bce0d606d1
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-10 13:00:09 +0300

    Update task_1.py

--- HT_2/task_1.py
+++ HT_2/task_1.py
@@ -1,9 +1,8 @@
 # 1. Write a script which accepts a sequence of
 # comma-separated numbers from user and generates a
 # list and a tuple with those numbers.
-print('Введіть числа через кому:')
 
-numbers = input()
+numbers = input('Введіть числа через кому:',)
 list_numbers = numbers.split(',')
 tuple_numbers = tuple(list_numbers)
 
