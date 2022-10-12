1cda69a86d4ead2845ba30161d71f8435742453c
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-12 23:12:04 +0300

    Create task_3.py

---
+++ HT_3/task_3.py
@@ -0,0 +1,15 @@
+# #3. Write a script to concatenate the following dictionaries to create a NEW one.
+#     # dict_1 = {'foo': 'bar', 'bar': 'buz'}
+#     # dict_2 = {'dou': 'jones', 'USD': 36}
+#     # dict_3 = {'AUD': 19.2, 'name': 'Tom'}
+
+
+dict_1 = {'foo': 'bar', 'bar': 'buz'}
+dict_2 = {'dou': 'jones', 'USD': 36}
+dict_3 = {'AUD': 19.2, 'name': 'Tom'}
+dict_4 = {}
+
+
+for dict in (dict_1, dict_2, dict_3):
+    dict_4.update(dict)
+print(dict_4)
