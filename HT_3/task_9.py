aa34f75710e51162a5fbc5e60739694ca948ddfa
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-12 23:20:39 +0300

    Create task_9.py

---
+++ HT_3/task_9.py
@@ -0,0 +1,24 @@
+# 9. Користувачем вводиться початковий і кінцевий рік. Створити цикл,
+# який виведе всі високосні роки в цьому проміжку (границі включно).
+# P.S. Рік є високосним, якщо він кратний 4, але не кратний 100,
+# а також якщо він кратний 400.
+
+
+try:
+    number = int(input('enter the year from:\n'))
+    n = int(input('enter the year to:\n'))
+    for number in range(n+1):
+        if number % 4 == 0:
+            if number != 100:
+                if number != 400:
+                    print(number)
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+
+
+except (TypeError, ValueError):
+    print("It's not a number")
