9dcce17466be140e3da6e89aabb1f483a53c28d6
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-13 20:47:12 +0300

    Update task_1.py

--- HT_3/task_1.py
+++ HT_3/task_1.py
@@ -6,7 +6,13 @@
 
 list_tuple = [(13, 21, 32), ('Sakura', 'Pego', 'Qwerty'), (2.52, 124.456, 1234.345), ()]
 m = input('Enther something:\n')
-new = [tuple(list(tup[:-1])+[m]) for tup in list_tuple]
+spare_list = []
 
 
-print(new)
+for i in range(len(spare_list)):
+    new_items = list(list_tuple[i])
+    new_items[-1] = m
+    list_tuple[i] = tuple(new_items)
+
+
+print(list_tuple)
