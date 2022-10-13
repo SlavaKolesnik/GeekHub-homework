a323d20a5f01bfabaf4ba6461eef65fffaed09fc
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-13 16:23:33 +0300

    Update task_1.py

--- HT_3/task_1.py
+++ HT_3/task_1.py
@@ -3,16 +3,10 @@
 # The "replacement" value is entered by user. The number of elements
 # n the tuples must be different.
 
-list_tuple = [(13, 21, 32), ('Sakura', 'Pego', 'Qwerty'), (2.52, 124.456, 1234.345)]
-lst, lst1, lst2 = [*list_tuple[0]], [*list_tuple[1]], [*list_tuple[2]]
-x = input('Enter something:\n')
+
+list_tuple = [(13, 21, 32), ('Sakura', 'Pego', 'Qwerty'), (2.52, 124.456, 1234.345), ()]
+m = input('Enther something:\n')
+new = [tuple(list(tup[:-1])+[m]) for tup in list_tuple]
 
 
-for l in lst, lst1, lst2:
-    del l[-1]
-    l.append(x)
-
-
-tpl, tpl1, tpl2 = tuple(lst), tuple(lst1), tuple(lst2)
-new_tuple_list = [tpl, tpl1, tpl2]
-print(new_tuple_list)
+print(new)
