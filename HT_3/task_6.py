b26da21bf85c459fd0b5c601810a32d26ab0efda
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-13 12:13:07 +0300

    Update task_6.py

--- HT_3/task_6.py
+++ HT_3/task_6.py
@@ -1,13 +1,20 @@
-#6. Write a script to get the maximum and minimum VALUE in a dictionary.
+# 6. Write a script to get the maximum and minimum VALUE in a dictionary.
 
 
 from pprint import pprint
 from termcolor import cprint
 
+
 super_dict = {
-    'Wolwerine': 'adamantium', 'Hawk eye': 'bow', 'Spider-man': 'web-shoters',
-        'Black-Widow': 'karate','Thor': 'mjolnir', 'Nick Fury': 'karate',
-            'Doomsdey': 'anger','Green Arrow': 'Bow', 'Hulk': 'anger'
+    'Wolwerine': 'adamantium',
+    'Hawk eye': 'bow',
+    'Spider-man': 'web-shoters',
+    'Black-Widow': 'karate',
+    'Thor': 'mjolnir',
+    'Nick Fury': 'karate',
+    'Doomsdey': 'anger',
+    'Green Arrow': 'Bow',
+    'Hulk': 'anger'
 }
 
 key_max = max(super_dict.keys(), key=(lambda k: super_dict[k]))
