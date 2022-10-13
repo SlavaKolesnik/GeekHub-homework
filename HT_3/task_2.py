8d4f95a24b6b12d58fd76dbe846d81911cb815e4
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-13 15:38:03 +0300

    Update task_2.py

--- HT_3/task_2.py
+++ HT_3/task_2.py
@@ -2,10 +2,7 @@
 # Test list: [(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []]
 
 test_list  = [(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []]
-
-res = list(map(list, test_list))
-list_1 = [x for x in res if x]
-list_2 = [x for x in list_1 if x != ['']]
+list_1 = [x for x in test_list if x]
 
 
-print(list_2)
+print(list_1)
