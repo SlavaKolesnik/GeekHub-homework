897e667b8cf94a7db1b3c8f96d165eb31a25f518
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-20 22:22:31 +0300

    Update task_5.py

--- HT_5/task_5.py
+++ HT_5/task_5.py
@@ -1,7 +1,6 @@
 # 5. Написати функцію <fibonacci>, яка приймає один аргумент і
 # виводить всі числа Фібоначчі, що не перевищують його.
 
-
 def fibonacci(n):
     if n >= 0:
         fib1 = 0
@@ -9,11 +8,10 @@ def fibonacci(n):
         print(fib1, fib2, end=' ')
         for i in range(0, n):
             fib1, fib2 = fib2, fib1 + fib2
-            if fib2 <= n:
+            if fib2 < n:
                 print(fib2, end=' ')
     else:
         print('Відємні числа не входять в послідовність!')
 
-        
-fibonacci(13)
 
+fibonacci(9)
