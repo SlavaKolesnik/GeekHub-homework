24755bddda932dee9001aca0c09de2907b44a197
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-20 17:58:15 +0300

    Update task_4.py

--- HT_5/task_4.py
+++ HT_5/task_4.py
@@ -4,13 +4,19 @@
 # введених даних та у випадку невідповідності - виведіть повідомлення.
 
 
-def prime_list():
-    start = int(input('Виберіть число від:\n'))
-    finish = int(input('Виберіть число до:\n'))
-    print(list(range(start, finish+1)))
+def prime_list(a, b):
+    if a < b:
+        lst = []
+        for i in range(a, b):
+            if i > 1:
+                for j in range(2, i):
+                    if i % j == 0:
+                        break
+                else:
+                    lst.append(i)
+        print(lst)
+    else:
+        print('Значення "a" не може бути більше "b", чи йому дорівнювати!')
 
 
-try:
-    prime_list()
-except ValueError:
-    print('Це не число!')
+prime_list(1, 1000)
