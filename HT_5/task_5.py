e735459643874ee47cfe2e3c1616e58492c2b42b
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-20 18:04:01 +0300

    Update task_5.py

--- HT_5/task_5.py
+++ HT_5/task_5.py
@@ -2,20 +2,18 @@
 # виводить всі числа Фібоначчі, що не перевищують його.
 
 
-def fibonacci():
-    fib1 = 0
-    fib2 = 1
-    n = int(input('Введіть число:\n'))
-    print(fib1, fib2, end=' ')
-    for i in range(0, n):
-        fib1, fib2 = fib2, fib1 + fib2
-        if fib2 <= n:
-            print(fib2, end=' ')
+def fibonacci(n):
+    if n >= 0:
+        fib1 = 0
+        fib2 = 1
+        print(fib1, fib2, end=' ')
+        for i in range(0, n):
+            fib1, fib2 = fib2, fib1 + fib2
+            if fib2 <= n:
+                print(fib2, end=' ')
+    else:
+        print('Відємні числа не входять в послідовність!')
 
-
-try:
-    fibonacci()
-except ValueError:
-    print('Це не число!')
-
+        
+fibonacci(13)
 
