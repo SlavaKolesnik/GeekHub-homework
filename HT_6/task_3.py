1f41c5cdbd1d566243aef6068334bd5a0705a53d
SlavaKolesnik <113468276+SlavaKolesnik@users.noreply.github.com>
2022-10-24 16:36:00 +0300

    Create task_3.py

---
+++ HT_6/task_3.py
@@ -0,0 +1,71 @@
+# 3. На основі попередньої функції (скопіюйте кусок коду) створити наступний скрипт:
+#    а) створити список із парами ім'я/пароль різноманітних видів (орієнтуйтесь по  
+#    правилам своєї функції) - як валідні, так і ні;
+#    б) створити цикл, який пройдеться по цьому циклу і, користуючись валідатором, перевірить 
+#    ці дані і надрукує для кожної пари значень відповідне повідомлення, наприклад:
+#       Name: vasya
+#       Password: wasd
+#       Status: password must have at least one digit
+#       -----
+#       Name: vasya
+#       Password: vasyapupkin2000
+#       Status: OK
+#    P.S. Не забудьте використати блок try/except ;)
+
+
+
+class LenPass(Exception):
+    pass
+
+
+class LenLog(Exception):
+    pass
+
+
+class Islower(Exception):
+    pass
+
+
+class Num(Exception):
+    pass
+
+
+log_pass = [['SobakaKusaka', 'a'], ['KotyaraCarapaka', 'MurnyamX#$%$@'],
+            ['Vo', 'Ay_Maugli1990'], ['KonyakaUdaryaka', 'gogo777%%%'],
+            ['GuskaShipaka', 'SHUUUU12$$$$']]
+
+
+def logpass(login, password):
+    if len(login) < 3 or len(login) > 50:
+        raise LenLog("ім'я повинно бути не меншим за 3 символа і не більшим за 50")
+
+    if len(password) < 8:
+        raise LenPass('пароль повинен бути не меншим за 8 символів')
+
+    if password.islower():
+        raise Islower('Пароль має бути з великої літери')
+
+    for i in password:
+        if i in '0123456789':
+            break
+    else:
+        raise Num('пароль повинен мати хоча б одну  цифру')
+
+
+for user in log_pass:
+    status = "OK"
+    try:
+        logpass(user[0], user[1])
+    except LenLog:
+        status = "ім'я повинно бути не меншим за 3 символа і не більшим за 50"
+    except LenPass:
+        status = 'пароль повинен бути не меншим за 8 символів'
+    except Islower:
+        status = 'Пароль має бути з великої літери'
+    except Num:
+        status = 'пароль повинен мати хоча б одну  цифру'
+    finally:
+        print(f'Name: {user[0]}')
+        print(f'Password: {user[1]}')
+        print(f'Status: {status}')
+        print('-----')
